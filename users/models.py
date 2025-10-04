150ae890a3c3b5840805076e0d959d90eb564e15
jobetcasquejo221 <jobetcasquejo221@gmail.com>
2025-10-04 01:46:20 +0000

    added new fields

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
@@ -10,10 +11,20 @@ class User(AbstractUser):
         ('Receptionist', 'Receptionist'),
         ('Patient', 'Patient'),
     )
+    GENDER_CHOICES = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+    )
 
     email = models.EmailField(_('email address'), unique=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
     employee_id = models.CharField(max_length=20, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
--- users/serializers.py
+++ users/serializers.py
@@ -4,7 +4,10 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'role', 'password')
+        fields = (
+            'id', 'username', 'first_name', 'last_name', 'email', 'role', 'password',
+            'phone_number', 'address', 'date_of_birth', 'profile_picture', 'gender'
+        )
         extra_kwargs = {'password': {'write_only': True}, 'role': {'read_only': True}}
 
     def create(self, validated_data):
